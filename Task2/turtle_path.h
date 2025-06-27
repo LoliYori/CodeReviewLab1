e097ba315d277b8cc836d43e0bc0716d3b43bc5a
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-27 12:37:16 +0500

    Update turtle_path.h

--- Task2/turtle_path.h
+++ Task2/turtle_path.h
@@ -1,6 +1,6 @@
 /**
  * @file turtle_path.h
- * @brief Заголовочный файл для задачи о черепашке
+ * @brief Г‡Г ГЈГ®Г«Г®ГўГ®Г·Г­Г»Г© ГґГ Г©Г« Г¤Г«Гї Г§Г Г¤Г Г·ГЁ Г® Г·ГҐГ°ГҐГЇГ ГёГЄГҐ
  */
 
 #ifndef TURTLE_PATH_H
@@ -14,14 +14,10 @@
 #include <ctime>
 #include <stdexcept>
 
-// FIXME: Добавлена константа максимального значения клетки
-// FIXME: Добавлена константа минимального размера доски
-// FIXME: Добавлена константа максимального размера доски
 const int MAX_CELL_VALUE = 100;  
 const int MIN_BOARD_SIZE = 2;    
 const int MAX_BOARD_SIZE = 80;    
 
-// FIXME: Изменены сигнатуры функций для возврата статуса ошибки
 bool ReadBoardFromFile(const std::string& filename, int& size, std::vector<std::vector<int>>& board);
 bool ReadBoardFromInput(int& size, std::vector<std::vector<int>>& board);
 bool GenerateRandomBoard(int& size, std::vector<std::vector<int>>& board);
