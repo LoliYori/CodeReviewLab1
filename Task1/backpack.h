5f91772ed9683ebeb5da293ea7b2289122383ea3
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-30 00:36:18 +0500

    Update backpack.h

--- Task1/backpack.h
+++ Task1/backpack.h
@@ -1,37 +1,40 @@
 /**
  * @file backpack.h
- * @brief Çàãîëîâî÷íûé ôàéë äëÿ ðåøåíèÿ çàäà÷è î ðþêçàêå
+ * @brief Заголовочный файл для решения задачи о рюкзаке
+ * @details Задача о выборе оптимального набора артефактов с учетом ограничений на вес и объем
  */
 
 #ifndef BACKPACK_H
 #define BACKPACK_H
 
-#include "backpack.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
 
- /**
-  * @struct Artifact
-  * @brief Ñòðóêòóðà, îïèñûâàþùàÿ àðòåôàêò
-  */
+/**
+ * @struct Artifact
+ * @brief Структура, описывающая артефакт
+ * @details Содержит информацию о весе и объеме артефакта
+ */
 struct Artifact {
-    int weight;
-    int volume;  
+    int weight; 
+    int volume; 
 };
 
 /**
- * @brief Ñðàâíèâàåò àðòåôàêòû ïî âåñó äëÿ ñîðòèðîâêè
- * @param a Ïåðâûé àðòåôàêò
- * @param b Âòîðîé àðòåôàêò
- * @return true, åñëè âåñ a ìåíüøå âåñà b
+ * @brief Сравнивает артефакты по весу для сортировки
+ * @param a Первый артефакт для сравнения
+ * @param b Второй артефакт для сравнения
+ * @return true, если вес артефакта a меньше веса артефакта b
  */
 bool SortWeight(const Artifact& a, const Artifact& b);
 
 /**
- * @brief Ðåøàåò çàäà÷ó î ðþêçàêå
- * @param filename Èìÿ âõîäíîãî ôàéëà ñ äàííûìè
+ * @brief Решает задачу о рюкзаке
+ * @param filename Имя входного файла с данными об артефактах
+ * @details Формат файла: первая строка - максимальный вес и объем,
+ *          последующие строки - вес и объем каждого артефакта
  */
 void SolveBackpackProblem(const std::string& filename);
 
